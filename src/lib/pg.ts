b98fc0cf1a8f17652a186dde0b854267935e0260
livecloud-labs <livecloud.store@gmail.com>
2024-08-16 22:17:27 +0100

    chore: update pg pool config

--- src/lib/pg.ts
+++ src/lib/pg.ts
@@ -1,11 +1,28 @@
 import { Pool } from 'pg';
+import { getLogger } from '../util/logger.util';
+
+const logger = getLogger(`pg-pool`);
+
+const RDS_ROOT_CERTIFICATE = process.env.RDS_ROOT_CERTIFICATE || '';
+const DB_PROXY_ENABLED = process.env.DB_PROXY_ENABLED === 'true';
+const DB_TLS_DISABLED = process.env.DB_TLS_DISABLED === 'true';
 
 let pgPool: Pool;
+
+const ssl = {
+  rejectUnauthorized: true,
+  ...(!DB_PROXY_ENABLED && { ca: RDS_ROOT_CERTIFICATE })
+};
 
 export function getPgPool(): Pool {
   if (pgPool) {
     return pgPool;
   }
+
+  logger.info('Creating pg pool', {
+    host: process.env.DB_HOST,
+    name: process.env.DB_NAME
+  });
 
   pgPool = new Pool({
     host: process.env.DB_HOST,
@@ -15,8 +32,18 @@ export function getPgPool(): Pool {
     password: process.env.DB_PASSWORD,
     max: Number(process.env.DB_MAX_CONNECTIONS),
     idleTimeoutMillis: Number(process.env.DB_IDLE_TIMEOUT_MS),
-    connectionTimeoutMillis: 2000
+    connectionTimeoutMillis: 2000,
+    ...(!DB_TLS_DISABLED && { ssl })
   });
 
   return pgPool;
 }
+
+process.on('SIGINT', async () => {
+  if (pgPool) {
+    await pgPool.end();
+    logger.info('PG pool ended through app termination');
+  }
+
+  process.exit(0);
+});

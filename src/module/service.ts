7d276bcd2ca1c85f4d0622c79ca8068d01a2794c
livecloud-labs <livecloud.store@gmail.com>
2024-10-31 12:24:12 +0000

    chore: remove legacy history code

--- src/module/service.ts
+++ src/module/service.ts
@@ -1,42 +1,7 @@
 import { Logger } from 'winston';
-import { RedisClient } from '@/lib/redis';
 import { PoolClient } from 'pg';
 import * as db from './db';
-import * as repository from './repository';
 import { MessageHistoryDbEntry } from './types';
-
-export async function getHistoryTtlhours(
-  logger: Logger,
-  pgClient: PoolClient,
-  nspRoomId: string
-): Promise<number> {
-  logger.info(`Getting history ttl`, { nspRoomId });
-
-  try {
-    const { rows } = await db.getHistoryTtlhours(pgClient, nspRoomId);
-
-    return rows[0].historyTtlHours;
-  } catch (err: any) {
-    logger.error(`Failed to get history ttl`, { err });
-    throw err;
-  }
-}
-
-export async function setHistoryTtl(
-  logger: Logger,
-  redisClient: RedisClient,
-  key: any,
-  historyTtlHours: number
-): Promise<void> {
-  logger.info(`Setting history ttl (${historyTtlHours}h)`, { key, historyTtlHours });
-
-  try {
-    await repository.setHistoryTtl(redisClient, key, historyTtlHours);
-  } catch (err: any) {
-    logger.error(`Failed to set history ttl`, { err });
-    throw err;
-  }
-}
 
 export function parseMessageHistoryDbEntries(
   logger: Logger,
